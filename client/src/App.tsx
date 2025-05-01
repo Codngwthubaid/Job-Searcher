f371abc3bd61f267560e7ce9018a05ed968c2af5
Codngwthubaid <codngwthubaid@gmail.com>
2025-05-01 20:29:15 +0530

    add routes for recruiter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import AppliedJobs from "./pages/apply-jobs";
 import Applications from "./pages/application";
+import RecruiterDashboard from "./pages/recruiter-dashboard";
+import ViewApplications from "./pages/view-applications";
+import ManageJobs from "./pages/manage-jobs";
+import AddJobs from "./pages/add-jobs";
 export default function App() {
 
   return (
@@ -10,6 +14,11 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/applied-jobs/:id" element={<AppliedJobs />} />
         <Route path={"/applications"} element={<Applications />} />
+        <Route path={"/dashboard"} element={<RecruiterDashboard />}>
+          <Route path={"add-job"} element={<AddJobs />} />
+          <Route path={"manage-job"} element={<ManageJobs />} />
+          <Route path={"view-applications"} element={<ViewApplications />} />
+        </Route>
       </Routes>
     </div>
   )
---
+++ client/src/pages/add-jobs.tsx
@@ -0,0 +1,7 @@
+export default function AddJobs() {
+    return (
+        <div>
+            <h1>AddJobs</h1>
+        </div>
+    );
+}
