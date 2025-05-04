eb2cc008c8e50c87c9e949562b5c758a8c53ab0f
Codngwthubaid <codngwthubaid@gmail.com>
2025-05-04 10:34:03 +0530

    update the mongoDB atlas url

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.post("/webhooks" , clerkWebhooks)
 
 const PORT = process.env.PORT || 5000
 
+
 await ConnectDB()
 app.listen(PORT, () => {
     console.log(`Server is running on port at http://localhost:${PORT}`)
